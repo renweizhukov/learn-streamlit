e832993516d17e784b5ae6a7a8ec8697181ce546
Wei Ren <wrn@amazon.com>
2024-09-11 14:51:22 -0700

    Highlight the maximum element of each column in the DataFrame table
    https://docs.streamlit.io/get-started/fundamentals/main-concepts#write-a-data-frame

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
+import numpy as np
 import pandas as pd
 
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
+st.dataframe(dataframe.style.highlight_max(axis=0))
