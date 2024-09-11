0c7d8ec2f68a2c9382eb513fb22c29e6a5e6229b
Wei Ren <wrn@amazon.com>
2024-09-11 14:47:02 -0700

    Display a DataFrame
    https://docs.streamlit.io/get-started/fundamentals/main-concepts#use-magic

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-import streamlit as st
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
-st.write("Hello World")
+import streamlit as st
+import pandas as pd
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
