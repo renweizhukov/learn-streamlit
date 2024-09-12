e126fb22618345da506325e0160728c5f631e6e0
Wei Ren <wrn@amazon.com>
2024-09-11 23:25:38 -0700

    Show progress layout
    https://docs.streamlit.io/get-started/fundamentals/main-concepts#show-progress

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+import time
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+'Starting a long computation...'
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
